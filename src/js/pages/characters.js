4b3250ecb31c87783ffbeb05660c5f14a9b74d62
Karina Mel <karinamelarantessilva@gmail.com>
2022-03-15 17:30:19 -0300

    Feat(JS,CSS,HTML)Slideshow, Page filters, character cards.

--- src/js/pages/characters.js
+++ src/js/pages/characters.js
@@ -1,3 +1,146 @@
-import { menu } from "../components/header.js";
+import dataGhibli from "../../data/ghibli/ghibli.js"
+import {sortArray} from "../../js/data.js"
 
-menu();
+const cardsContainer = document.createElement("section");
+let personagens = sortArray.filterSpecies("")
+
+//O paramentro da funcao nada mais e do que so um exemplo do dado ou seja, oque vc vai tratar, o js realmente vai fazer funcionar quando passar dados de verdade pra ele, linha 35.
+ function renderScreen(data) {
+  document.getElementById("mainContainer").appendChild(cardsContainer);
+  cardsContainer.classList.add("cardsContainer");
+  cardsContainer.innerHTML = "";
+  console.log(data)
+  if (data == undefined) {
+    return
+  }
+
+     data.map((characters) => {
+     const card = document.createElement("ul");
+     const name = document.createElement("h1");
+     const img = document.createElement("img");
+     const divName = document.createElement("div");
+
+     cardsContainer.appendChild(card);
+     divName.appendChild(name);
+     card.appendChild(divName);
+     card.appendChild(name);
+     card.appendChild(img);
+
+     card.dataset.modal = characters.id
+     img.dataset.modal = characters.id
+     name.dataset.modal = characters.id
+
+     card.classList.add("cards");
+     name.classList.add("nameCharacters");
+     img.classList.add("img");
+     divName.classList.add("divname");
+
+      img.src = characters.img;
+      name.innerHTML = characters.name;
+
+      card.addEventListener("click", (e) => {
+        dataGhibli.films.forEach((itemAtual) => { itemAtual.people.forEach((i)=> {if (i.id == e.target.dataset.modal) {
+          renderModal(i); }})});});
+  })}
+
+renderScreen(sortArray.searchCharacters(""))
+
+document.getElementById("inputAZ").addEventListener("change", (e) => {
+  renderScreen(sortArray.filterCharactersAZ(e.target.value,personagens));
+
+});
+
+document.querySelector("#search").addEventListener("keyup", (e) => {
+  document.querySelector("#calculoCharaters").remove()
+  InsertCalc(sortArray.searchCharacters(e.target.value).length)
+  renderScreen(sortArray.searchCharacters(e.target.value));
+  personagens = sortArray.searchCharacters(e.target.value)
+});
+
+document.getElementById("inputFilmes").addEventListener("change", (e) => {
+  const calculoFilms = sortArray.compareMovies(e.target.value)
+  const calculo =  100 * calculoFilms.length / 171
+  renderScreen(calculoFilms);
+  document.querySelector("#calculoCharaters").remove()
+  personagens = calculoFilms
+  if(calculoFilms.length != 171){
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoCharaters"> ${e.target.value} has ${calculo.toFixed(2)}% characters </p>`)
+  } else{
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoCharaters">there are ${calculo.toFixed(2)}% characters in total </p>`)
+  }
+
+});
+
+document.getElementById("inputSelect").addEventListener("change", (e) => {
+
+  const calculoFilms = sortArray.filterSpecies(e.target.value)
+  renderScreen(calculoFilms);
+  document.querySelector("#calculoCharaters").remove()
+  personagens = calculoFilms
+  InsertCalc(calculoFilms.length)
+
+});
+
+const modalContainer = document.querySelector(".modalContainer");
+
+function renderModal(infoFilme) {
+
+  const modal = document.querySelector(".modal");
+
+  const filmes =  dataGhibli.films.filter(itematual => {
+    return itematual.people.filter((personagemAtual) => {
+      return personagemAtual.id == infoFilme.id
+    })
+  })
+  console.log(filmes)
+  modal.innerHTML = "";
+
+  modal.innerHTML = `
+  <img class="closeModal" src="./assets/Vector.png" alt="closeButton">
+    <h1>${infoFilme.name}</h1>
+      <img class="poster" src="${infoFilme.img}">
+          <h3>Age:${infoFilme.age}</h3>
+          <h3>Gender: ${infoFilme.gender}</h3>
+          <h3> Specie: ${infoFilme.specie}</h3>
+          <h3>Hair Color: ${infoFilme.hair_color}</h3>
+          <h3>Eye Color: ${infoFilme.eye_color}</h3>
+
+  `;
+  const buttonCloseModal = document.querySelector(".closeModal");
+  buttonCloseModal.addEventListener("click", () => {
+    modalContainer.classList.remove("showModal");
+
+  });
+
+  modalContainer.classList.add("showModal");
+}
+
+
+
+
+
+
+function InsertCalc(calculoFilms){
+
+  let calculo =  100 * calculoFilms / 171
+
+  if(calculo.length == 171){
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoCharaters">This species has .${calculo.toFixed(2)}% of characters </p>`)
+  } else{
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoCharaters">there are ${calculo.toFixed(2)}% of characters in total </p>`)
+  }
+
+
+}
+
+
+InsertCalc(sortArray.filterSpecies("").length)
+
+
+sortArray.filterFilmsDropDown().forEach(function(newFilter){
+  document.querySelector('#inputFilmes').insertAdjacentHTML('beforeend', `<option value="${newFilter}" class= "dropdown">${newFilter}`)
+})
+
+sortArray.filterSpeciesDropDown().forEach(function(newFilter){
+  document.querySelector('#inputSelect').insertAdjacentHTML('beforeend', `<option value="${newFilter}" class= "dropdown">${newFilter}`)
+})
--- src/js/pages/movies.js
+++ src/js/pages/movies.js
@@ -2,9 +2,10 @@ import dataGhibli from "../../data/ghibli/ghibli.js";
 import { sortArray } from "../data.js";
 
 const cardsContainer = document.createElement("section");
+const modalContainer = document.querySelector(".modalContainer");
 
 
-sortArray.search("Castle");
+
 export function renderScreen(data) {
   document.getElementById("mainContainer").appendChild(cardsContainer);
   cardsContainer.classList.add("cardsContainer");
@@ -25,6 +26,10 @@ export function renderScreen(data) {
     card.appendChild(divTitle);
     cardsContainer.appendChild(card);
 
+    card.dataset.modal = items.id
+    poster.dataset.modal = items.id
+    title.dataset.modal = items.id
+
     card.classList.add("cards");
     title.classList.add("titleWithinCard");
     poster.classList.add("poster");
@@ -32,91 +37,98 @@ export function renderScreen(data) {
     poster.src = items.poster;
     poster.title = items.title;
     title.innerHTML = items.title;
-  });
-}
-renderScreen(dataGhibli);
-document.querySelector("#search").addEventListener("keyup", (e) => {
 
+
+    card.addEventListener("click", (e) => {
+      dataGhibli.films.forEach((ItemAtual) => {
+        if (ItemAtual.id == e.target.dataset.modal) {
+          renderModal(ItemAtual);
+        }
+      });
+    });
+  });
+
+}
+
+renderScreen(dataGhibli);
+
+document.querySelector("#search").addEventListener("keyup", (e) => {
+   if(e.target.value != ""){
+   document.querySelector("#calculoFilms").style.display = "none"}else{
+    document.querySelector("#calculoFilms").style.display = "initial"
+   }
   renderScreen(sortArray.search(e.target.value));
+});
+
+document.getElementById("directors").addEventListener("change", (e) => {
+  const calculoFilms = sortArray.toComparerProducerAndDirector(e.target.value,'director')
+  renderScreen(calculoFilms);
+  document.querySelector("#calculoFilms").remove()
+  if(calculoFilms.length == 20){
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoFilms">The amount of films the studio owns is ${calculoFilms.length}</p>`)
+  } else{
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoFilms">The amount of films the Directors owns is ${calculoFilms.length}</p>`)
+  }
+});
+
+document.getElementById("Producer").addEventListener("change", (e) => {
+
+  const calculoFilms = sortArray.toComparerProducerAndDirector(e.target.value,'producer')
+  renderScreen(calculoFilms);
+  document.querySelector("#calculoFilms").remove()
+  if(calculoFilms.length == 20){
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoFilms">The amount of films the studio owns is ${calculoFilms.length}</p>`)
+  } else{
+    document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoFilms">The amount of films the Producer owns is ${calculoFilms.length}</p>`)
+  }
 });
 
 document.getElementById("inputSelect").addEventListener("change", (e) => {
   renderScreen(sortArray.filterArray(e.target.value));
 });
 
-// export function renderScreen(data) {
-//   document.getElementById("mainContainer").appendChild(cardsContainer);
-//   cardsContainer.classList.add("cardsContainer");
-//   cardsContainer.innerHTML = "";
-//   data.films.map((items) => {
-//     cardsContainer.innerHTML = `
-//         <ul class="cards">
-//         <img class="poster" src="${items.poster}">
-//         <div class="divTitle">
-//         <h1 class="titleWithinCard">${items.title}</h1>
-//         </div>
-//         </ul>
 
-//     `;
-//     //const card = document.createElement("ul");
-//     //const title = document.createElement("h1");
-//     //const poster = document.createElement("img");
-//     //const divTitle = document.createElement("div");
 
-//     // divTitle.appendChild(title);
-//     //card.appendChild(poster);
-//     //card.appendChild(divTitle);
-//     //cardsContainer.appendChild(card);
+const calculoFilms = dataGhibli.films.length
+document.querySelector(".inputContainer").insertAdjacentHTML('afterend', `<p id="calculoFilms">The amount of films the studio owns is ${calculoFilms}</p>`)
 
-//     card.dataset.modal = items.id;
-//     title.dataset.modal = items.id;
-//     poster.dataset.modal = items.id;
-//     divTitle.dataset.modal = items.id;
 
-//     card.classList.add("cards");
-//     title.classList.add("titleWithinCard");
-//     poster.classList.add("poster");
-//     divTitle.classList.add("divTitle");
 
-//     //poster.src = items.poster;
-//     //poster.title = items.title;
-//     //title.innerHTML = items.title;
 
-//     card.addEventListener("click", (e) => {
-//       dataGhibli.films.forEach((ItemAtual) => {
-//         if (ItemAtual.id == e.target.dataset.modal) {
-//           renderModal(ItemAtual);
-//         }
-//       });
-//     });
-//   });
-// }
-// renderScreen(dataGhibli);
-// document.getElementById("inputSelect").addEventListener("change", (e) => {
-//   sortArray.filterArray(e.target.value);
-// });
 
-// const modalContainer = document.querySelector(".modalContainer");
+sortArray.search("Castle");
 
-// function renderModal(infoFilme) {
-//   const modal = document.querySelector(".modal");
-//   modal.innerHTML = "";
+sortArray.filterCreatorProducerAndDirector('producer').forEach(function(newFilter){
+  document.querySelector("#Producer").insertAdjacentHTML('beforeend', `<option value="${newFilter}" class= "dropdown">${newFilter}`)
+})
 
-//   modal.innerHTML = `
-//     <h1>${infoFilme.title}</h1>
-//       <img src="${infoFilme.poster}">
-//         <p>${infoFilme.description}</p>
-//           <h3>${infoFilme.director}</h3>
-//           <h3>${infoFilme.producer}</h3>
-//           <h3>${infoFilme.release_date}</h3>
-//           <h3> Score ${infoFilme.rt_score}</h3>
-//             <button class="closeModal">Close</button>
+sortArray.filterCreatorProducerAndDirector('director').forEach(function(newFilter){
+  document.querySelector("#directors").insertAdjacentHTML('beforeend', `<option value="${newFilter}" class= "dropdown">${newFilter}`)
+})
 
-//   `;
-//   const buttonCloseModal = document.querySelector(".closeModal");
-//   buttonCloseModal.addEventListener("click", () => {
-//     modalContainer.classList.remove("showModal");
-//   });
+function renderModal(infoFilme) {
+  const modal = document.querySelector(".modal");
 
-//   modalContainer.classList.add("showModal");
-// }
+
+  modal.innerHTML = "";
+
+  modal.innerHTML = `
+  <img class="closeModal" src="./assets/Vector.png" alt="closeButton">
+    <h1>${infoFilme.title}</h1>
+      <img class="poster" src="${infoFilme.poster}">
+        <p>Description: ${infoFilme.description}</p>
+
+          <h3>Director:${infoFilme.director}</h3>
+          <h3>Producer: ${infoFilme.producer}</h3>
+          <h3>Release Date: ${infoFilme.release_date}</h3>
+          <h3> Score: ${infoFilme.rt_score}</h3>
+
+  `;
+  const buttonCloseModal = document.querySelector(".closeModal");
+  buttonCloseModal.addEventListener("click", () => {
+    modalContainer.classList.remove("showModal");
+
+  });
+
+  modalContainer.classList.add("showModal");
+}
